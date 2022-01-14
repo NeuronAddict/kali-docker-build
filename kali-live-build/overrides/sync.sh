811313dd564d1afb4f3eeeecf58a557d135e5162
neuronaddict <manufrommars@elacourarie.org>
2022-01-14 11:33:55 +0100

    override support

---
+++ kali-live-build/overrides/sync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+
+if [[ -n "$OVERRIDE_PROFILE" ]]
+then
+  profile=$OVERRIDE_PROFILE
+else
+  [[ $# -lt 2 ]] && echo "syntaxe: $0 <profile> (or set OVERRIDE_PROFILE env var)" && exit 0
+  profile=$1
+fi
+
+echo "Override with profile ${profile}"
+
+cp -prv "/overrides/${profile}/"* /live-build-config/
